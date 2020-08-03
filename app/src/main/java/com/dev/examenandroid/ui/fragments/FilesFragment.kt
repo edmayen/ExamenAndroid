c5a9404060251348e246875ffcd8cfd2df8cc813
edmayen <ed_mayen@hotmail.com>
2020-08-03 15:46:43 -0500

    Manipulacion de archivo

---
+++ app/src/main/java/com/dev/examenandroid/ui/fragments/FilesFragment.kt
@@ -0,0 +1,180 @@
+package com.dev.examenandroid.ui.fragments
+
+import android.app.DownloadManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.dev.examenandroid.R
+import kotlinx.android.synthetic.main.fragment_files.*
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+
+class FilesFragment : Fragment() {
+
+    private val PERMISSION_REQUEST_CODE = 1234
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_files, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var isUnzip: Boolean = false
+
+        btnCallService.setOnClickListener {
+
+        }
+
+        btnDownloadZipFile.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    // Permission Granted
+                    downloadFile()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        PERMISSION_REQUEST_CODE
+                    )
+                }
+            } else {
+                // Less than M
+                downloadFile()
+            }
+        }
+
+        btnOpenZipFile.setOnClickListener {
+            val path = context?.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)!!.path+"/"
+            val filename = "employees_data.json.zip"
+            isUnzip = unpackZip(path, filename)
+        }
+
+        btnReadZipFile.setOnClickListener {
+            if(isUnzip){
+                readFile()
+                showToast("File found.")
+            }
+            else{
+                showToast("File not found.")
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    downloadFile()
+                } else {
+                    showToast("Permission Denied...")
+                }
+            }
+        }
+    }
+
+    private fun downloadFile() {
+        val url = "https://firebasestorage.googleapis.com/v0/b/example-e6943.appspot.com/o/employees_data.json.zip?alt=media&token=02daec6d-cd37-48eb-bfa5-da5862f40b97"
+        try {
+            val request = DownloadManager.Request(Uri.parse(url))
+            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+            request.setTitle("Download")
+            request.setDescription("Downloading your file")
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalFilesDir(
+                requireContext(),
+                Environment.DIRECTORY_DOWNLOADS,
+                "/employees_data.json.zip"
+            )
+
+            val downloadManager = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            downloadManager.enqueue(request)
+
+            showToast("Download complete.")
+
+        } catch (e: Exception) {
+            Log.d("Download", e.toString())
+            showToast(e.toString())
+        }
+    }
+
+    private fun unpackZip(path: String, zipname: String): Boolean {
+        val inputMan: InputStream
+        val zis: ZipInputStream
+        try {
+            var ze: ZipEntry? = null
+            var filename: String
+            val buffer = ByteArray(1024)
+            var count: Int
+
+            inputMan = FileInputStream(path + zipname)
+            zis = ZipInputStream(BufferedInputStream(inputMan))
+
+            while (zis.nextEntry.also { ze = it } != null) {
+                filename = ze!!.name
+                if (ze!!.isDirectory) {
+                    val fmd = File(path + filename)
+                    fmd.mkdirs()
+                    continue
+                }
+                val fout = FileOutputStream(path + filename)
+                while (zis.read(buffer).also { count = it } != -1) {
+                    fout.write(buffer, 0, count)
+                }
+                fout.close()
+                zis.closeEntry()
+            }
+            zis.close()
+        } catch (e: IOException) {
+            Log.d("UNZIP", e.toString())
+            e.printStackTrace()
+            return false
+        }
+        showToast("Unzip complete.")
+        return true
+    }
+
+    private fun readFile(){
+        try {
+            val bufferedReader: BufferedReader = File(context?.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)!!.path +"/employees_data.json").bufferedReader()
+            val inputString = bufferedReader.use { it.readText()}
+            parseJson(inputString)
+        }catch (e: IOException){
+            Log.d("READ", e.toString())
+            e.printStackTrace()
+        }
+    }
+
+    private fun parseJson(inputString: String){
+        Log.d("JSON", inputString)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+    }
+}
--- app/src/main/java/com/dev/examenandroid/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/dev/examenandroid/ui/fragments/HomeFragment.kt
@@ -41,16 +41,6 @@ import java.util.zip.ZipInputStream
 
 class HomeFragment : Fragment() {
 
-    private val STORAGE_PERMISSION_CODE: Int = 1000
-    lateinit var authHome: FirebaseAuth
-
-    private val viewModel by viewModels<ColaboradoresViewModel> { ColaboradoresViewModelFactory(
-        ColaboradorRepositoryImpl(
-        DataSource(ColaboradoresDatabase.invoke(requireActivity().applicationContext))
-    )
-    )
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -66,8 +56,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val apiService = RetrofitInstance
-
         btnLogout.setOnClickListener {
             // authHome.signOut()
             findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
@@ -82,103 +70,8 @@ class HomeFragment : Fragment() {
         }
 
         btnConsultaServicio.setOnClickListener {
-            //callService()
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if(ContextCompat.checkSelfPermission(requireContext(), WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                    requestPermissions(arrayOf(WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
-                } else {
-                    downloadZip()
-                }
-            } else{
-                downloadZip()
-            }
+            findNavController().navigate(R.id.action_homeFragment_to_filesFragment)
         }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            STORAGE_PERMISSION_CODE -> {
-                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    downloadZip()
-                } else {
-                    Toast.makeText(requireContext(), "Permiso denegado", Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-    }
-
-
-     fun downloadZip(){
-         val url = "https://firebasestorage.googleapis.com/v0/b/example-e6943.appspot.com/o/employees_data.json.zip?alt=media&token=02daec6d-cd37-48eb-bfa5-da5862f40b97"
-         val request = DownloadManager.Request(
-             Uri.parse(url))
-             .setTitle("Descargando...")
-             .setDescription("Descargando Archivo...")
-             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-             .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-             .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOCUMENTS,"/employees_data.json.zip")
-
-        var manager = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        manager.enqueue(request)
-
-        val path = context?.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!!.path+"/"
-        val filename = "employees_data.json.zip"
-        if(unpackZip(path, filename)){
-            readFile()
-        }
-    }
-
-    private fun unpackZip(path: String, zipname: String): Boolean {
-        val inputMan: InputStream
-        val zis: ZipInputStream
-        try {
-            var ze: ZipEntry
-            var filename: String
-            val buffer = ByteArray(2048)
-            var count: Int
-
-            inputMan = FileInputStream(path + zipname)
-            zis = ZipInputStream(BufferedInputStream(inputMan))
-
-            while (zis.nextEntry.also { ze = it } != null) {
-                filename = ze.name
-                if (ze.isDirectory) {
-                    val fmd = File(path + filename)
-                    fmd.mkdirs()
-                    continue
-                }
-                val fout = FileOutputStream(path + filename)
-                while (zis.read(buffer).also { count = it } != -1) {
-                    fout.write(buffer, 0, count)
-                }
-                fout.close()
-                zis.closeEntry()
-            }
-            zis.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return false
-        }
-        return true
-    }
-
-    private fun readFile(){
-        try {
-            val bufferedReader: BufferedReader = File(context?.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!!.path +"/employees_data.json").bufferedReader()
-            val inputString = bufferedReader.use { it.readText()}
-            parseJson(inputString)
-        }catch (e:IOException){
-            e.printStackTrace()
-        }
-    }
-
-    private fun parseJson(inputString: String){
-        Log.d("JSON", inputString)
     }
 
     private fun callService(){
